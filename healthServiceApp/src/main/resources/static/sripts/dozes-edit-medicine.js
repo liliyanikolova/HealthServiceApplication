163834c9e76e4dc147a6d06843604e76a80119e8
liliyanikolova <lilyanikolova@gmail.com>
2017-05-07 22:53:48 +0300

    Refactor - extract scripts as sepatate files

---
+++ healthServiceApp/src/main/resources/static/sripts/dozes-edit-medicine.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Administrator on 5/7/2017.
+ */
+
+var btn_add_doze    = document.getElementById( 'btn-add-doze' );
+var btn_remove_doze = document.createElement( 'button' );
+var doze_input      = document.createElement( 'input' );
+btn_remove_doze.type      = 'button';
+btn_remove_doze.innerText = 'Изтрий';
+doze_input.type           = 'text';
+btn_remove_doze.classList.add('btn');
+btn_remove_doze.classList.add('btn-primary');
+
+var doze_inputs     = document.getElementsByClassName( 'doze-input' );
+var doze_input_tmpl = doze_inputs[ 0 ];
+
+btn_add_doze.addEventListener( 'click', addDoze );
+
+function addDoze () {
+    var doze_input_clone      = doze_input_tmpl.cloneNode( true );
+    var btn_remove_doze_clone = btn_remove_doze.cloneNode( true );
+
+    doze_input_clone.getElementsByTagName( 'input' )[ 0 ].value      = '';
+    doze_input_clone.appendChild( btn_remove_doze_clone );
+
+    btn_remove_doze_clone.addEventListener( 'click', deleteDoze.bind( null, doze_input_clone ) );
+
+    btn_add_doze.parentNode.insertBefore( doze_input_clone, btn_add_doze );
+
+    doze_input_clone.getElementsByTagName( 'input' )[ 0 ].focus();
+}
+
+function deleteDoze ( doze_input, event ) {
+    doze_input.remove();
+}
